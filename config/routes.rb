47fa109d6c97ce542768d73719c029173ab17eb5
polina_akmaeva <polinaakmaeva575@gmail.com>
2023-06-25 17:34:48 +0300

    Create form (/hostels/new): load jquery, add script, add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :hostels
+  resources :hostels 
+  
+  # Маршруты для экшенов загрузки списков
+  get '/countries', to: 'hostels#countries'
+  get '/regions', to: 'hostels#regions'
+  get '/towns', to: 'hostels#towns'
+
   root 'home#index'
 end
