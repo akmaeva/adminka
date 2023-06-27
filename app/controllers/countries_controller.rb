94a03a9e769d4046a0f20614f7320d53bc06b021
polina_akmaeva <polinaakmaeva575@gmail.com>
2023-06-28 00:52:47 +0300

    Added countries page

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,6 +1,17 @@
 class CountriesController < ApplicationController
+
+    def index
+        @countries = Country.all
+    end
+
     def new
         @country = Country.new
+    end
+
+    def edit
+    end
+
+    def show
     end
 
     def create
@@ -17,6 +28,30 @@ class CountriesController < ApplicationController
                 format.html { render :new, status: :unprocessable_entity }
                 format.json { render json: @country.errors, status: :unprocessable_entity }
             end
+        end
+    end
+
+    def update
+        respond_to do |format|
+            if Country.exists?(name: @country.name)
+                format.html { redirect_to countries_url, notice: "Country already exists." }
+                format.json { render json: { error: "Country already exists." }, status: :unprocessable_entity }
+            elsif @country.update(country_params)
+                format.html { redirect_to country_url(@country), notice: "Country was successfully updated." }
+                format.json { render :show, status: :ok, location: @country }
+            else
+                format.html { render :edit, status: :unprocessable_entity }
+                format.json { render json: @country.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+
+    def destroy
+        @country.destroy
+    
+        respond_to do |format|
+          format.html { redirect_to countries_url, notice: "Country was successfully destroyed." }
+          format.json { head :no_content }
         end
     end
 
